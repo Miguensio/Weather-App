dd2b5981f87c52b1aafaf7616ee1b4d7c0f2d77f
Miguel Raaz <miguelraazbarajas@gmail.com>
2025-01-21 08:22:58 -0400

    Better input styling

--- src/components/input.js
+++ src/components/input.js
@@ -9,7 +9,7 @@ function Input({ onCitySubmit }){
 
   return(
     <div className="input-group p-4 w-50 m-auto input-container">
-        <input type="text" id="input" className="form-control text-center bg-primary-subtle" placeholder="Input the city of your choice"/>
+        <input type="text" id="input" className="city-input form-control text-center bg-primary-subtle" placeholder="Input the city of your choice"/>
 				<div className="input-group-btn">
 					<button className="btn btn-default bg-primary-subtle mx-2" onClick={userInput}>
 						Search
